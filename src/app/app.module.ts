7c1c8ab053b02062f1034f0b8166f4fd7398b110
BAferiel <feriel.benabdelghani@esprit.tn>
2023-12-09 15:57:37 +0100

    crud reclamation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReclamationComponent } from './reclamation/reclamation.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { ReclamationModule } from './reclamation/reclamation.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ReclamationComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    ReclamationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
